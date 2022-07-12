c33386381fc1ec6da456b86000397ee79c2ecaf2
t-jeswang <107151882+t-jeswang@users.noreply.github.com>
2022-07-12 12:52:28 -0700

    'iframe1'

--- WebApplication4/wwwroot/js/all.js
+++ WebApplication4/wwwroot/js/all.js
@@ -1,2 +1,8 @@
 ﻿document.getElementById("clientKey").style.display = "none";
 document.getElementById("type").style.display = "none";
+
+var x = document.getElementsByClassName("status-container");
+x.querySelector("status").addEventListener("beforeprint", function () {
+    top.postMessage(document.getElementById('status'));
+});
+
