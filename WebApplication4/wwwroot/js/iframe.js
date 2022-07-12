883a166584691c83947cbfcaefb562949a54a3bc
t-jeswang <107151882+t-jeswang@users.noreply.github.com>
2022-07-12 13:58:47 -0700

    'iframejs3'

--- WebApplication4/wwwroot/js/iframe.js
+++ WebApplication4/wwwroot/js/iframe.js
@@ -1,5 +1,6 @@
 ﻿var x = document.getElementById("status");
+console.log(x.innerText);
 x.addEventListener("load", function () {
-    console.log('sent the message!')
-    top.postMessage(document.getElementById('status'));
+    console.log('sent the message!');
+    top.postMessage(document.getElementById('status').innerText);
 });
